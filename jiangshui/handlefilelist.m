02395b8faaa83ea869039417be6c1a35b95c9283
wucangeo <wucangeo@gmail.com>
2016-02-18 23:38:30 +0800

    完成批量写入mysql

--- jiangshui/handlefilelist.m
+++ jiangshui/handlefilelist.m
@@ -1,7 +1,19 @@
-function fileList = handlefilelist(  )
+function fileList = handlefilelist( dirin )
 %HANDLEFILELIST Summary of this function goes here
 %   Detailed explanation goes here
 
+dirData = dir(dirin);
+dirIndex = [dirData.isdir];  %# Find the index for directories
+fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
+if ~isempty(fileList)
+    fileListIn = cellfun(@(x) fullfile(dirin,x),fileList,'UniformOutput',false);  %# Prepend path to files
+end
+
+for i = 1:length(fileListIn)
+    %     fprintf('%s\n',fileListIn{i});
+    %     fprintf('%s\n',fileListOut{i});
+    txtimport(fileListIn{i});
+end
 
 end
 
