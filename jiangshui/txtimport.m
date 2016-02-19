c0acc42354673a1073de0b389cfe3bd6065bdd72
wucangeo <wucangeo@gmail.com>
2016-02-20 00:28:17 +0800

    处理2014年数据

--- jiangshui/txtimport.m
+++ jiangshui/txtimport.m
@@ -10,7 +10,7 @@ raindatestr = strcat('20',filename(1:6));   % ???
 raindate = strcat('20',filename(1:2),'-',filename(3:4),'-',filename(5:6));  %??--
 
 conn =database('jiangshui','root','noroot','com.mysql.jdbc.Driver','jdbc:mysql://localhost:3306/jiangshui');
-tablename = 'rain2013';
+tablename = 'rain2014';
 colnames = {'m_date','m_dates','sid','m_x','m_y','m_z','m_value'};
 if isopen(conn)
     for i = 1:m
